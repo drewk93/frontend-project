e5ab744f800011ace654451058d382123d9681ea
Andrew Kullman <agkullman@protonmail.com>
2023-10-25 09:36:29 -0800

    Commit 4 - Date Extraction Complete

--- project.js
+++ project.js
@@ -7,7 +7,7 @@
 Chess.Com API Endpoints: 
 
 1) Looking up Archive of Games: https://api.chess.com/pub/player/{username}/games/archives
-2) Looking up games by year and month: https://api.chess.com/pub/player/{username}/games/2009/10/
+2) Looking up games by year and month: https://api.chess.com/pub/player/{username}/games/
 3) *Looking up games PGN by year and month: https://api.chess.com/pub/player/erik/games/2009/10/pgn''
   // Note that the PGN for 3) is not a JSON
 
@@ -25,25 +25,25 @@ TO DO
 
 
 
-// function extractMoves(pgn) {
-//     const lines = pgn.trim().split('\n');
-//     const movesLine = lines.filter(line => line.trim() !== '' && !line.startsWith('[')).pop();
-//     return movesLine;
-// }
+function extractMoves(pgn) {
+    const lines = pgn.trim().split('\n');
+    const movesLine = lines.filter(line => line.trim() !== '' && !line.startsWith('[')).pop();
+    return movesLine;
+}
 
-// loadState()
+// extractPGN()
 
-// function loadState(){
-//     $.get('https://api.chess.com/pub/player/erik/games/archives', function(data){
-//     console.log(data)
-//     // const pgn = data.games[0].pgn;
-//     // const moves = extractMoves(pgn);
-//     // console.log(moves); // This will log only the moves.
-//     // // chess.js load state and PGN notation conversion to FEN
-//     // let gameState = new Chess()
-//     // gameState.load_pgn(moves)
-//     // let fen = gameState.fen()
-//     // let board1 = ChessBoard('board1', fen)
+// function extractPGN(){
+//     $.get('https://api.chess.com/pub/player/erik/games/2009/10/', function(data){
+//     console.log(data.games)
+//     const pgn = data.games[0].pgn;
+//     const moves = extractMoves(pgn);
+//     console.log(moves); // This will log only the moves.
+//     // chess.js load state and PGN notation conversion to FEN
+//     let gameState = new Chess()
+//     gameState.load_pgn(moves)
+//     let fen = gameState.fen()
+//     let board1 = ChessBoard('board1', fen)
     
 // },'json').fail(function(error) {
 //     console.error("Request failed:", error);
@@ -55,15 +55,32 @@ $(document).ready(function() {
 });
 
 function loadEventListeners(){
-
 const $input = $('input');
 const $submit = $('#submit');
 
+    
     $submit.on('click', function(e){
+        e.preventDefault();
         const inputVal = $("input[name='search']").val().trim()
+        //Check if input is empty
+        if (!inputVal){
+            alert("Please enter a username before hitting submit");
+            return;
+        }
+
         const url = `https://api.chess.com/pub/player/${inputVal}/games/archives`
-        $.get(url, function(data){
-            console.log(data)
-        })
+               $.get(url, function(data){
+            data.archives.forEach((item) => {
+               let parts = item.split("games/")
+                    if (parts.length > 1){
+                        const dates = parts[1];
+                        console.log(dates)
+                    }
+                }
+            )
+            
+        }).fail(function() {
+            alert("Failed to retrieve data. Please try again.");
+        });
     })
 }
